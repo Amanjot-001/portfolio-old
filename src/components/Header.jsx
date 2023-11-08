3d9883585baf86d05da1a3924cef1b6edd0a26a2
Amanjot-001 <amanjotsingh2309@gmail.com>
2023-11-08 12:05:47 +0530

    header created

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import '../assets/styles/Header.css'
+
+export default function Header() {
+    return (
+        <div className="header">
+            <div className="marquee"></div>
+            <div className="bg-1"></div>
+            <div className="bg-2"></div>
+            <div className="main">
+                <div className="greet">
+                    <p>Hi I'M AMANJOT SINGH</p>
+                </div>
+                <div className="info">
+                    <p>
+                        Enthusiastic computer science engineering student deeply passionate about programming, web development, and software engineering. Join me as I unveil a world of skills and projects in the realm of technology.
+                    </p>
+                </div>
+                <div className="socials"></div>
+            </div>
+        </div>
+    )
+}
