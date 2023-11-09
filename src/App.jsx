59896ae48374802c3e10f95d0022b2c22064ca43
Amanjot-001 <amanjotsingh2309@gmail.com>
2023-11-09 12:01:30 +0530

    work label added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Header from './components/Header'
+import Label from './components/Label'
 
 function App() {
     return (
         <div id="app">
             <Header />
+            <Label title={'WORK'} />
         </div>
     )
 }
---
+++ src/components/Label.jsx
@@ -0,0 +1,9 @@
+import '../assets/styles/Label.css'
+
+export default function Label({title}) {
+    return (
+        <div className="label">
+            <h1>{title}</h1>
+        </div>
+    )
+}
