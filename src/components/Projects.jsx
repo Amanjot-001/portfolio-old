02e58c868beab5ee015b71fb5c2dafa2b7cf6eef
Amanjot-001 <amanjotsingh2309@gmail.com>
2023-11-09 14:25:34 +0530

    style update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,15 +2,32 @@ import '../assets/styles/Projects.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlay } from '@fortawesome/free-solid-svg-icons'
 
+const projects = [
+    {
+        name: 'vyzon'
+    },
+    {
+        name: 'galaxy'
+    },
+    {
+        name: 'semicolon'
+    },
+    {
+        name: 'code convertor'
+    },
+]
+
 export default function Projects() {
     return (
         <div className="projects">
             <div className="nav">
                 <div className="nav-content">
-                    <div className="nav-item">
-                    <FontAwesomeIcon icon={faPlay} />
-                    Vyzon &nbsp; &nbsp;
-                    </div>
+                    {projects.map((project, index) => (
+                        <div key={index} className="nav-item">
+                        <FontAwesomeIcon icon={faPlay} />
+                        {project.name} &nbsp; &nbsp;
+                      </div>
+                    ))}
                 </div>
             </div>
             <div className="project-show"></div>
