2ec07294277d44bb2621b807ac951b6f2dcc8546
Amanjot-001 <amanjotsingh2309@gmail.com>
2023-11-25 23:22:10 +0530

    btn effect added

--- src/components/SocialBtn.jsx
+++ src/components/SocialBtn.jsx
@@ -1,7 +1,25 @@
 import '../assets/styles/SocialBtn.css'
+import { useState } from 'react'
 
-export default function SocialBtn({value, href}) {
+export default function SocialBtn({ value, href }) {
+    const [isClicked, setIsClicked] = useState(false);
+
+    const handleClick = (event) => {
+        setIsClicked(true);
+        event.target.classList.add('clicked');
+        setTimeout(() => {
+            setIsClicked(false);
+            event.target.classList.remove('clicked');
+        }, 1000);
+    };
+
     return (
-            <a className='btn' href={href}>{value}</a>
+        <a
+            className='btn'
+            href={href}
+            onClick={handleClick}
+        >
+            {value}
+        </a>
     )
 }
